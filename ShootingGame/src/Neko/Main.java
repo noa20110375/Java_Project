55fe4073fb85719fe20254def3d87fc7015e9fcf
noa20110375 <112079550+noa20110375@users.noreply.github.com>
2022-11-02 14:35:22 +0900

    Delete ShootingGame directory

--- ShootingGame/src/Neko/Main.java
+++
@@ -1,9 +0,0 @@
-package Neko;
-public class Main {
-    public static final int SCREEN_WIDTH = 1280;
-    public static final int SCREEN_HEIGHT = 720;
-
-    public static void main(String[] args) {
-        new ShootingGame();
-    }
-}
